f6d5fb37f0e35c1a17ba61a7a107753a5552dd86
alexno-sherlock <alexno@stanford.edu>
2020-11-02 05:07:06 -0800

    Add SscWscDataModule container

--- datasets/__init__.py
+++ datasets/__init__.py
@@ -1,5 +1,5 @@
 from .isrc import ISRCDataset
 from .khc import KoreanDataset
-from .ssc_wsc_psg import SscWscPsgDataset
+from .ssc_wsc_psg import SscWscPsgDataset, SscWscDataModule
 
 __all__ = ["ISRCDataset", "KoreanDataset", "SscWscPsgDataset"]
