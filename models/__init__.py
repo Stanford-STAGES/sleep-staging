c463f13396da5b6ae284ad78bf3221882ee6cb27
alexno-sherlock <alexno@stanford.edu>
2020-11-02 05:08:55 -0800

    Fix models imports

--- models/__init__.py
+++ models/__init__.py
@@ -1,9 +1,7 @@
 from .massc import MasscModel
-from .massc_v2 import MasscV2Model
+from .massc_avgloss import AvgLossMasscModel
+from .massc_simple import SimpleMasscModel
 from .stages import StagesModel
+from .utime import UTimeModel
 
-__all__ = [
-    "MasscModel",
-    "MasscV2Model",
-    "StagesModel",
-]
+__all__ = ["AvgLossMasscModel", "MasscModel", "SimpleMasscModel", "StagesModel", "UTimeModel"]
