9ae27dfbd485ae739404b2a20501d1fff66c92d7
alexno-sherlock <alexno@stanford.edu>
2020-11-02 04:48:28 -0800

    Change F1 score to macro

--- utils/evaluate_performance.py
+++ utils/evaluate_performance.py
@@ -56,9 +56,9 @@ def evaluate_performance(record_predictions, evaluation_windows=[1, 3, 5, 10, 15
             acc = accuracy_score(t, p)
             bal_acc = balanced_accuracy_score(t, p)
             kappa = cohen_kappa_score(t, p)
-            f1 = f1_score(t, p, average="micro")
-            prec = precision_score(t, p, average="micro")
-            recall = recall_score(t, p, average="micro")
+                f1 = f1_score(t, p, average="macro")
+                prec = precision_score(t, p, average="macro")
+                recall = recall_score(t, p, average="macro")
             mcc = matthews_corrcoef(t, p)
 
             # Assign metrics to dataframe
