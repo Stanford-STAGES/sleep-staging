af47ad193b9ae25dfbd5df911987e62e940c8043
alexno-sherlock <alexno@stanford.edu>
2020-12-03 05:59:39 -0800

    Added SimpleBlock

--- models/layers.py
+++ models/layers.py
@@ -3,6 +3,33 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 
+class SimpleBlock(nn.Module):
+    def __init__(self, n_filters_in, n_filters, kernel_size, strided=False):
+        super().__init__()
+        self.filters_in = n_filters_in
+        self.filters = n_filters
+        self.kernel_size = kernel_size
+        self.padding = kernel_size // 2
+        self.stride = 2 if strided else 1
+
+        self.block = nn.Sequential(
+            nn.Conv1d(
+                in_channels=self.filters_in,
+                out_channels=self.filters,
+                kernel_size=self.kernel_size,
+                stride=self.stride,
+                padding=self.padding,
+                bias=False,
+            ),
+            nn.BatchNorm1d(self.filters),
+            nn.ReLU(),
+        )
+
+    def forward(self, x):
+        z = self.block(x)
+        return z
+
+
 class AdditiveAttention(nn.Module):
     """See https://arxiv.org/pdf/1809.10932.pdf"""
 
