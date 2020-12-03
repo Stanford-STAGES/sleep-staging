bfd44f8b76025cf23c773432ab9eeeca7275d4df
alexno-sherlock <alexno@stanford.edu>
2020-12-03 06:04:03 -0800

    Update to output metrics and confusion matrix

--- predict.py
+++ predict.py
@@ -97,9 +97,16 @@ def run_predict():
     ds_args["n_records"] = args.n_records
     ds_args["n_workers"] = args.n_workers
     ds_args["limit_test_batches"] = args.limit_test_batches
-    test_dm.append(("SSC-WSC", datasets.SscWscDataModule(**ds_args)),)
-    for dm in test_dm:
-        dm[1].setup("test")
+
+    test_dm.append(("SSC-WSC_eval", datasets.SscWscDataModule(**ds_args)),)
+    test_dm[0][1].setup("fit")
+
+    # test_dm.append(("SSC-WSC_test", datasets.SscWscDataModule(**ds_args)),)
+    # test_dm[0][1].setup("test")
+
+    # for dm in test_dm:
+    # dm[1].setup("test")
+    # dm[1].setup("fit")
     # model.dataset_params["n_records"] = 5
 
     # test_datasets.append(("SSC-WSC", datasets.SscWscPsgDataset(**model.dataset_params)))
@@ -117,7 +124,13 @@ def run_predict():
     # for name, tdl in test_dataloaders:
     for name, tdm in test_dm:
         # predictions = trainer.test(model, test_dataloaders=tdl, verbose=False)[0]
-        predictions = trainer.test(model, datamodule=tdm, verbose=False)
+        # predictions = trainer.test(model, datamodule=tdm, verbose=False)
+
+        try:
+            predictions = trainer.test(model, test_dataloaders=tdm.val_dataloader(), verbose=False)
+        except AttributeError:
+            predictions = trainer.test(model, test_dataloaders=tdm.test_dataloader(), verbose=False)
+
         # trainer.test(model, datamodule=tdm, verbose=False)
         if not model.use_ddp or (model.use_ddp and torch.distributed.get_rank() == 0):
             # with open(os.path.join(results_dir, f"{name}_predictions.pkl"), "rb") as pkl:
@@ -126,27 +139,52 @@ def run_predict():
             with open(os.path.join(results_dir, f"{name}_predictions.pkl"), "wb") as pkl:
                 pickle.dump(predictions, pkl)
 
-            df, cm_sub, cm_tot = utils.evaluate_performance(predictions, evaluation_windows=[1, 15, 30], cases=["all"])
+            df, cm_sub, cm_tot = utils.evaluate_performance(predictions, evaluation_windows=[1], cases=["all"])
             with np.printoptions(precision=3, suppress=True):
+                s = ""
                 for eval_window in cm_tot.keys():
-                    print()
-                    print(f"Evaluation window - {eval_window}")
-                    print(f"---------------------------------")
+                    # print()
+                    s += "\n"
+                    s += f"Evaluation window - {eval_window} s\n"
+                    s += "---------------------------------\n"
                     for case in cm_tot[eval_window].keys():
-                        print(f"Case: {case}")
-                        print(cm_tot[eval_window][case])
+                        df_ = df.query(f'Window == "{eval_window} s" and Case == "{case}"')
+                        s += f"Case: {case}\n"
+                        s += f"{cm_tot[eval_window][case]}\n"
                         NP = cm_tot[eval_window][case].sum(axis=1)
                         PP = cm_tot[eval_window][case].sum(axis=0)
-                        precision = np.diag(cm_tot[eval_window][case]) / PP
-                        recall = np.diag(cm_tot[eval_window][case]) / NP
-                        f1 = 2 * precision * recall / (precision + recall)
-                        print(f"F1: {f1}")
-                        print()
+                        N = cm_tot[eval_window][case].sum()
+                        precision = np.diag(cm_tot[eval_window][case]) / (PP + 1e-10)
+                        recall = np.diag(cm_tot[eval_window][case]) / (NP + 1e-10)
+                        f1 = 2 * precision * recall / (precision + recall + 1e-10)
+                        acc = np.diag(cm_tot[eval_window][case]).sum() / N
+
+                        pe = N ** (-2) * (NP @ PP)
+                        kappa = 1 - (1 - acc) / (1 - pe)
+
+                        c = np.diag(cm_tot[eval_window][case]).sum()
+                        mcc = (c * N - NP @ PP) / (np.sqrt(N ** 2 - (PP @ PP)) * np.sqrt(N ** 2 - (NP @ NP)))
+
+                        s += "\n"
+                        s += f'Precision:\t{df_["Precision"].mean():.3f} +/- {df_["Precision"].std():.3f} \t|\t{precision}\n'
+                        s += f'Recall:\t\t{df_["Recall"].mean():.3f} +/- {df_["Recall"].std():.3f} \t|\t{recall}\n'
+                        s += f'F1: \t\t{df_["F1"].mean():.3f} +/- {df_["F1"].std():.3f} \t|\t{f1}\n'
+                        s += (
+                            f'Accuracy:\t{df_["Accuracy"].mean():.3f} +/- {df_["Accuracy"].std():.3f} \t|\t{acc:.3f}\n'
+                        )
+                        s += f'Kappa:\t\t{df_["Kappa"].mean():.3f} +/- {df_["Kappa"].std():.3f} \t|\t{kappa:.3f}\n'
+                        s += f'MCC:\t\t{df_["MCC"].mean():.3f} +/- {df_["MCC"].std():.3f} \t|\t{mcc:.3f}\n'
+                        s += "\n"
+                        # print(f"F1: {f1}")
+                        # print()
+            print(s)
+            with open(os.path.join(results_dir, f"{name}_results.txt"), "w") as txt_file:
+                print(s, file=txt_file)
             # pprint.pprint(cm_tot[1])
             # pprint.pprint(cm_tot[30])
             df.to_csv(os.path.join(results_dir, f"{name}_results.csv"))
             with open(os.path.join(results_dir, f"{name}_confusionmatrix.pkl"), "wb") as pkl:
-                pickle.dump({"confusiomatrix_subject": cm_sub, "confusiomatrix_total": cm_tot}, pkl)
+                pickle.dump({"confusionmatrix_subject": cm_sub, "confusionmatrix_total": cm_tot}, pkl)
 
     # def run_testing(_data, _name):
     #     predictions = trainer.test(model, test_dataloaders=_data, verbose=False)
