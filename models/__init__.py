c00bd6acd34ca502983e8b8d8bc5f21a7d1afa8d
alexno-sherlock <alexno@stanford.edu>
2020-12-03 06:02:13 -0800

    Scope new models in __init__

--- models/__init__.py
+++ models/__init__.py
@@ -1,7 +1,18 @@
 from .massc import MasscModel
+from .massc_attention import MASSCAttention
+from .massc_average import MASSCAverage
 from .massc_avgloss import AvgLossMasscModel
+from .massc_new import MasscV2Model
 from .massc_simple import SimpleMasscModel
 from .stages import StagesModel
 from .utime import UTimeModel
 
-__all__ = ["AvgLossMasscModel", "MasscModel", "SimpleMasscModel", "StagesModel", "UTimeModel"]
+available_models = {
+    "massc": MasscModel,
+    "stages": StagesModel,
+    "utime": UTimeModel,
+    "massc_attention": MASSCAttention,
+    "massc_average": MASSCAverage,
+}
+
+# __all__ = ["available_models"]
