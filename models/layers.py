da47fa0f96852278fd3d52f9b6e0e1c21dd7ac2b
alexno-sherlock <alexno@stanford.edu>
2020-12-03 06:01:53 -0800

    Added BottleneckBlodk

--- models/layers.py
+++ models/layers.py
@@ -3,6 +3,46 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 
+class BottleneckBlock(nn.Module):
+    def __init__(self, n_filters_in, n_filters, kernel_size, strided=False):
+        super().__init__()
+        self.filters_in = n_filters_in
+        self.filters = n_filters
+        self.kernel_size = kernel_size
+        self.padding = kernel_size // 2
+        self.stride = 2 if strided else 1
+        self.net = nn.Sequential(
+            nn.BatchNorm1d(self.filters_in),
+            nn.ReLU(),
+            nn.Conv1d(in_channels=self.filters_in, out_channels=self.filters, kernel_size=1, bias=False),
+            nn.BatchNorm1d(self.filters),
+            nn.ReLU(),
+            nn.Conv1d(
+                in_channels=self.filters,
+                out_channels=self.filters,
+                kernel_size=self.kernel_size,
+                padding=self.padding,
+                stride=self.stride,
+                bias=False,
+            ),
+            nn.BatchNorm1d(self.filters),
+            nn.ReLU(),
+            nn.Conv1d(in_channels=self.filters, out_channels=self.filters * 4, kernel_size=1, bias=False),
+        )
+        self.projection = nn.Conv1d(
+            in_channels=self.filters_in,
+            out_channels=self.filters * 4,
+            kernel_size=self.stride,
+            stride=self.stride,
+            bias=False,
+        )
+
+    def forward(self, x):
+        z = self.net(x)
+        # print(x.shape)
+        return self.projection(x) + z
+
+
 class ResidualBlock(nn.Module):
     # TODO: Fix this
     def __init__(self, n_filters_in, n_filters_out, kernel_size, strided=False, projection_type="identity"):
@@ -114,6 +154,22 @@ class AdditiveAttention(nn.Module):
 
 if __name__ == "__main__":
     x = torch.rand([32, 2, 5 * 60 * 128])
+    blocks = nn.Sequential(
+        ResidualBlock(2, 4, 3, strided=True, projection_type="identity"),
+        ResidualBlock(4, 4, 3, strided=False, projection_type="identity"),
+        ResidualBlock(4, 8, 3, strided=True, projection_type="identity"),
+        ResidualBlock(8, 8, 3, strided=False, projection_type="identity"),
+    )
+    z = blocks(x)
+    print(z.shape)
+
+    # ResBlock = ResidualBlock(5, 8, 3, strided=False, projection_type="identity")
+    # z = ResBlock(x)
+
+    # ResBlock = ResidualBlock(5, 8, 9, strided=True, projection_type="identity")
+    # z = ResBlock(x)
+    # print(z.shape)
+    # print("Hej")
     # att = AdditiveAttention(1024, 256)
     # c, alpha = att(x, 30)
     # print(c)
