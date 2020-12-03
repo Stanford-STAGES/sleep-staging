dd9bf76bd17f83eb20b85db14ff1bee9980e71d3
alexno-sherlock <alexno@stanford.edu>
2020-12-03 05:40:22 -0800

    Added visibility for datamodules

--- datasets/__init__.py
+++ datasets/__init__.py
@@ -1,5 +1,10 @@
 from .isrc import ISRCDataset
 from .khc import KoreanDataset
-from .ssc_wsc_psg import SscWscPsgDataset, SscWscDataModule
+from .ssc_wsc_psg import SscWscDataModule
+from .stages import STAGESDataModule
 
-__all__ = ["ISRCDataset", "KoreanDataset", "SscWscPsgDataset"]
+available_datamodules = {
+    "stages": STAGESDataModule,
+    "ssc-wsc": SscWscDataModule,
+}
+# __all__ = ["ISRCDataset", "KoreanDataset", "SscWscPsgDataset"]
