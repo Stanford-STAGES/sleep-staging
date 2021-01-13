29fb3528da82ecb33031c50481da37ed63200dc5
alexno-sherlock <alexno@stanford.edu>
2021-01-13 01:49:08 -0800

    Rename KoreanDataset to KHCDataModule

--- datasets/__init__.py
+++ datasets/__init__.py
@@ -1,10 +1,12 @@
-from .isrc import ISRCDataset
-from .khc import KoreanDataset
+# from .isrc import ISRCDataModule
+from .khc import KHCDataModule
 from .ssc_wsc_psg import SscWscDataModule
 from .stages import STAGESDataModule
 
 available_datamodules = {
     "stages": STAGESDataModule,
     "ssc-wsc": SscWscDataModule,
+    "khc": KHCDataModule,
+    # "isrc": ISRCDataModule,
 }
 # __all__ = ["ISRCDataset", "KoreanDataset", "SscWscPsgDataset"]
