fc553f867733b4e4c859c6ab18c98bf469dbaedb
alexno-sherlock <alexno@stanford.edu>
2021-01-13 02:13:37 -0800

    Comment out unnecessary code

--- train.py
+++ train.py
@@ -1,6 +1,5 @@
 import os
 import pickle
-import pprint
 
 # from argparse import ArgumentParser
 # from datetime import datetime
@@ -39,104 +38,81 @@ def run_training():
 
     args = utils.get_args()
 
-    # If you wish to view applied settings, uncomment these two lines.
-    # pprint.pprint(vars(args))
-    # return
-
     # Remember to seed!
-    if args.model_type == "stages":
-        seed = 42 + int(args.model_name.split("_")[-1])
-    else:
-        seed = 1337
-    seed_everything(seed)
+    seed_everything(args.seed)
 
     # Setup data module for training
-    if args.model_type == "stages":
-        dm = datasets.STAGESDataModule(**vars(args))
-        dm.setup()
-    else:
-        dm = datasets.SscWscDataModule(**vars(args))
-        dm.setup()
-        args.cb_weights = dm.train.dataset.cb_weights
+    dm, args = utils.get_data(args)
+    # if args.model_type == "stages":
+    #     dm = datasets.STAGESDataModule(**vars(args))
+    #     dm.setup()
+    # else:
+    #     dm = datasets.SscWscDataModule(**vars(args))
+    #     dm.setup()
+    #     args.cb_weights = dm.train.dataset.cb_weights
     # return
 
     # Setup model
     model = utils.get_model(args)
-    # if args.model_type == "stages":
-    #     # Update some hyperparameters
-    #     if args.model_name.split("_")[1] == "rh":
-    #         args.n_hidden_units = np.random.randint(256, 256 + 128)  # np.random.randint(0.5 * 256, 1.5 * 256 + 1)
-    #     model = StagesModel(**vars(args))
-    # elif args.model_type == "massc":
-    #     model = MasscModel(**vars(args))
-    # elif args.model_type == "simple_massc":
-    #     model = SimpleMasscModel(**vars(args))
-    # elif args.model_type == "avgloss_massc":
-    #     model = AvgLossMasscModel(**vars(args))
-    # elif args.model_type == "utime":
-    #     model = UTimeModel(**vars(args))
-    # else:
-    #     raise NotImplementedError
 
     # Setup callbacks
-    # wandb_logger_params = dict(save_dir="experiments", project="sleep-staging", log_model=True)
-    wandb_logger_params = dict(save_dir=args.save_dir, project="sleep-staging", log_model=False)
-    try:
-        csv_logger_params = dict(save_dir="./experiments", version=args.save_dir.parts[-1])
-    except AttributeError:
-        # csv_logger_params = dict(save_dir="./experiments", version=args.save_dir.split("/")[-1])
-        csv_logger_params = dict(
-            save_dir="./experiments",
-            name=os.path.join(*args.save_dir.split("/")[1:-1]),
-            version=args.save_dir.split("/")[-1],
-        )
-    # print(args.save_dir.split('/'))
-    # return
-    # if args.model_type == "stages":
-    #     wandb_logger_params.update(dict(name=args.model_name))
-    #     csv_logger_params.update(dict(name=args.model_name))
-    # elif args.model_type in ["massc", "massc_v2", "simple_massc", "avgloss_massc", "utime"]:
-    # wandb_logger_params.update(dict(name=args.model_type))
-    try:
-        wandb_logger_params.update(dict(name=os.path.join(*args.save_dir.parts[1:])))
-    except AttributeError:
-        wandb_logger_params.update(dict(name=os.path.join(*args.save_dir.split("/")[1:])))
-    checkpoint_monitor = pl_callbacks.ModelCheckpoint(
-        filepath=os.path.join(args.save_dir, "{epoch:03d}-{eval_loss:.2f}"),
-        monitor="eval_loss",
-        save_last=True,
-        save_top_k=1,
-    )
-    # csv_logger_params.update(dict(name=args.save_dir.split("/")[1]))
-    # tb_logger = pl_loggers.TensorBoardLogger("logs/")
-    # csv_logger_params.update(dict(name=args.save_dir.parts[1]))
+    loggers, callbacks = utils.get_loggers_callbacks(args, model)
+    # # wandb_logger_params = dict(save_dir="experiments", project="sleep-staging", log_model=True)
+    # wandb_logger_params = dict(save_dir=args.save_dir, project="sleep-staging", log_model=False)
+    # try:
+    #     csv_logger_params = dict(save_dir="./experiments", version=args.save_dir.parts[-1])
+    # except AttributeError:
+    #     # csv_logger_params = dict(save_dir="./experiments", version=args.save_dir.split("/")[-1])
+    #     csv_logger_params = dict(
+    #         save_dir="./experiments",
+    #         name=os.path.join(*args.save_dir.split("/")[1:-1]),
+    #         version=args.save_dir.split("/")[-1],
+    #     )
+    # # print(args.save_dir.split('/'))
+    # # return
+    # # if args.model_type == "stages":
+    # #     wandb_logger_params.update(dict(name=args.model_name))
+    # #     csv_logger_params.update(dict(name=args.model_name))
+    # # elif args.model_type in ["massc", "massc_v2", "simple_massc", "avgloss_massc", "utime"]:
+    # # wandb_logger_params.update(dict(name=args.model_type))
+    # try:
+    #     wandb_logger_params.update(dict(name=os.path.join(*args.save_dir.parts[1:])))
+    # except AttributeError:
+    #     wandb_logger_params.update(dict(name=os.path.join(*args.save_dir.split("/")[1:])))
+    # checkpoint_monitor = pl_callbacks.ModelCheckpoint(
+    #     filepath=os.path.join(args.save_dir, "{epoch:03d}-{eval_loss:.2f}"),
+    #     monitor="eval_loss",
+    #     save_last=True,
+    #     save_top_k=1,
+    # )
+    # # csv_logger_params.update(dict(name=args.save_dir.split("/")[1]))
+    # # tb_logger = pl_loggers.TensorBoardLogger("logs/")
+    # # csv_logger_params.update(dict(name=args.save_dir.parts[1]))
 
-    # lr_monitor = pl_callbacks.LearningRateMonitor()
-    csv_logger = pl_loggers.CSVLogger(**csv_logger_params)
-    if args.debug:
-        wandb_logger_params.update(dict(offline=True))
-    wandb_logger = pl_loggers.WandbLogger(**wandb_logger_params)
-    wandb_logger.watch(model)
-    # if not args.debug:
-    #     wandb_logger = WandbLogger(**wandb_logger_params)
-    #     wandb_logger.watch(model)
-    # else:
-    #     wandb_logger = None
-    # wandb_logger = None
-    loggers = [
-        csv_logger,
-        # tb_logger,
-        wandb_logger,
-    ]
-    callbacks = [
-        pl_callbacks.EarlyStopping(monitor="eval_loss", patience=args.earlystopping_patience),
-        pl_callbacks.LearningRateMonitor(),
-    ]
+    # # lr_monitor = pl_callbacks.LearningRateMonitor()
+    # csv_logger = pl_loggers.CSVLogger(**csv_logger_params)
+    # if args.debug:
+    #     wandb_logger_params.update(dict(offline=True))
+    # wandb_logger = pl_loggers.WandbLogger(**wandb_logger_params)
+    # wandb_logger.watch(model)
+    # # if not args.debug:
+    # #     wandb_logger = WandbLogger(**wandb_logger_params)
+    # #     wandb_logger.watch(model)
+    # # else:
+    # #     wandb_logger = None
+    # # wandb_logger = None
+    # loggers = [
+    #     csv_logger,
+    #     # tb_logger,
+    #     wandb_logger,
+    # ]
+    # callbacks = [
+    #     pl_callbacks.EarlyStopping(monitor="eval_loss", patience=args.earlystopping_patience),
+    #     pl_callbacks.LearningRateMonitor(),
+    # ]
 
     # Define trainer object from arguments
-    trainer = Trainer.from_argparse_args(
-        args, deterministic=True, checkpoint_callback=checkpoint_monitor, logger=loggers, callbacks=callbacks
-    )
+    trainer = Trainer.from_argparse_args(args, deterministic=True, logger=loggers, callbacks=callbacks)
 
     # ================================================================================================================
     # LEARNING RATE FINDER ROUTINE
@@ -151,7 +127,7 @@ def run_training():
     # Fit model using trainer
     trainer.fit(model, dm)
 
-    # return 0
+    return 0
 
     # Return results on eval data
     predictions = trainer.test(
