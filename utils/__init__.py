8927887ca54e1be9743474ae2f868f1cd73861e4
Alexander Neergaard Olesen <aneol@dtu.dk>
2021-02-23 13:21:36 +0100

    Sanititize utils init. Added multitaper, more plotting

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,19 +1,33 @@
 from .arg_utils import get_args
 from .chunking import chunks
 from .dataset_utils import get_data
-from .evaluate_performance import evaluate_performance
-from .evaluate_performance import transition_matrix
-from .h5_utils import get_h5_info
-from .h5_utils import load_h5_data
-from .h5_utils import load_psg_h5_data
+from .evaluate_performance import (
+    evaluate_performance,
+    transition_matrix,
+)
+from .h5_utils import (
+    get_h5_info,
+    load_h5_data,
+    load_psg_h5_data,
+    initialize_record,
+    get_stable_sleep_periods,
+    get_stable_stage,
+    get_unknown_stage,
+    SCALERS,
+)
 from .logger_callback_utils import get_loggers_callbacks
+from .multitaper_spectrogram import multitaper_spectrogram
 from .model_utils import get_model
 from .parallel_bar import ParallelExecutor
-from .plotting import plot_segment
+from .plotting import (
+    plot_segment, 
+    plot_psg_hypnogram_hypnodensity,
+    extended_epoch_view
+)
 
 read_fns = {
     "cc": load_h5_data,
     "raw": load_psg_h5_data,
 }
 
-__all__ = ["evaluate_performance", "load_h5_data", "ParallelExecutor", "read_fns", "get_args", "get_model"]
+# __all__ = ["evaluate_performance", "load_h5_data", "ParallelExecutor", "read_fns", "get_args", "get_model"]
