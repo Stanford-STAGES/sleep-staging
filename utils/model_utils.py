f16806a907fd83df71b46793d4bfb80bc0e0fe2e
alexno-sherlock <alexno@stanford.edu>
2021-04-09 03:16:08 -0700

    fix instantiating bug when loading from ckpt

--- utils/model_utils.py
+++ utils/model_utils.py
@@ -8,13 +8,16 @@ import models
 def get_model(args):
 
     if args.resume_from_checkpoint:
-        # resume_from_checkpoint = args.resume_from_checkpoint
-        # args = Namespace(
-        #     **(torch.load(args.resume_from_checkpoint, map_location=torch.device("cpu"))["hyper_parameters"])
-        # )
-        # args.resume_from_checkpoint = resume_from_checkpoint
-        model = models.available_models[args.model_type].load_from_checkpoint(args.resume_from_checkpoint)
-    else:
+        resume_from_checkpoint = args.resume_from_checkpoint
+        args = Namespace(
+            **(torch.load(args.resume_from_checkpoint, map_location=torch.device("cpu"))["hyper_parameters"])
+        )
+        args.resume_from_checkpoint = resume_from_checkpoint
+        # model = models.available_models[args.model_type].load_from_checkpoint(args.resume_from_checkpoint)
+    # else:
+    try:
         model = models.available_models[args.model_type](**vars(args))
+    except TypeError:
+        model = models.available_models[args.model_type](vars(args))
 
     return model
