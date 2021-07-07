a6857074b7c1fa5fe907d0c1331fb580a1b5b061
alexno-sherlock <alexno@stanford.edu>
2021-07-07 08:25:01 -0700

    add hypnogram load functions

--- utils/sta_utils.py
+++ utils/sta_utils.py
@@ -9,6 +9,7 @@ def load_hypnogram_ihc(sta_file):
         # THIS IS A TEMPORARY HACK
         head, tail = os.path.split(sta_file)
         tail = "".join(s.upper() if i in set([5, 6]) else s for i, s in enumerate(tail))
+
         sta_file = os.path.join(head, tail)
     if not os.path.exists(sta_file):
         return None
@@ -32,7 +33,7 @@ def load_hypnogram_jcts(sta_file):
     if not os.path.exists(sta_file):
         return None
     with open(sta_file, "r") as fp:
-        hyp = np.loadtxt(fp).astype(np.uint32)[:, 0, np.newaxis]
+        hyp = np.loadtxt(fp).astype(np.uint32)[:, 1, np.newaxis]
 
     hypnogram = np.zeros(hyp.shape, dtype=np.uint32)
     hypnogram[hyp == 0] = 1
@@ -72,13 +73,12 @@ def load_hypnogram_khc(sta_file):
             hyp = np.loadtxt(fp).astype(np.uint32)[:, 1, np.newaxis]
     except:
         _, tail = os.path.split(sta_file)
-        sta_file = os.path.join('data', 'khc', 'hypnogram', sta_file)
+        sta_file = os.path.join("data", "khc", "hypnogram", tail)
 
     if not os.path.exists(sta_file):
         return None
     with open(sta_file, "r") as fp:
         hyp = np.loadtxt(fp).astype(np.uint32)[:, 1, np.newaxis]
-
 
     hypnogram = np.zeros(hyp.shape, dtype=np.uint32)
     hypnogram[hyp == 0] = 1
@@ -117,28 +117,37 @@ def load_hypnogram_default(sta_file):
 
     return hyp
 
+
 hypnogram_read_fns = {
-    'dhc': load_hypnogram_dhc,
-    'jcts': load_hypnogram_jcts,
-    'ihc': load_hypnogram_ihc,
-    'wsc': load_hypnogram_default,
-    'ssc': load_hypnogram_default,
-    'khc': load_hypnogram_default,
-    'stages-stnf': None,
-    'stages-bogn': None,
-    'stages-gs': None,
-    'stages-gsdv': None,
-    'stages-mayo': None,
-    'stages-msmi': None,
-    'stages-msnf': None,
-    'stages-msqw': None,
-    'stages-msth': None,
-    'stages-mstr': None,
-    'stages-stlk': None,
+    "dhc": load_hypnogram_dhc,
+    "jcts": load_hypnogram_jcts,
+    "ihc": load_hypnogram_ihc,
+    "wsc": load_hypnogram_default,
+    "ssc": load_hypnogram_default,
+    "khc": load_hypnogram_khc,
+    "ahc": None,
+    "stages-stnf": None,
+    "stages-bogn": None,
+    "stages-gs": None,
+    "stages-gsdv": None,
+    "stages-mayo": None,
+    "stages-msmi": None,
+    "stages-msnf": None,
+    "stages-msqw": None,
+    "stages-msth": None,
+    "stages-mstr": None,
+    "stages-stlk": None,
 }
 
 
-def load_scored_data(sta_file, cohort=None):
+def load_scored_data(fileid, cohort=None):
+
+    if hypnogram_read_fns[cohort] is None:
+        return np.ones((3000, 1), dtype=np.uint32) * 7
+
+    sta_file = fileid + ".STA"
+    if not os.path.exists(sta_file):
+        sta_file = os.path.join("data", cohort, "hypnogram", os.path.split(fileid)[1].split(".")[0] + ".STA")
 
     hyp = hypnogram_read_fns[cohort](sta_file)
     hyp = np.concatenate([hyp, np.ones((2000, hyp.shape[1]), dtype=np.uint32) * 7], axis=0)
